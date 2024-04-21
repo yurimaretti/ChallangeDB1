3b936aaa5bc3b8abf44a8ba9f6b678de2f81ec2e
yurimaretti <yurimaretti@hotmail.com>
2024-04-21 12:03:41 -0300

    Refatoração do código, criando o componente Card, que será usado de template nas demais telas do projeto

---
+++ app/src/main/java/br/com/fiap/challangedb1/components/Card.kt
@@ -0,0 +1,24 @@
+package br.com.fiap.challangedb1.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import br.com.fiap.challangedb1.R
+
+@Composable
+fun CardTemplate(conteudo: @Composable () -> Unit) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = CardDefaults
+            .cardColors(colorResource(id = R.color.cor_card)),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        conteudo()
+    }
+}
--- app/src/main/java/br/com/fiap/challangedb1/screens/LoginScreen.kt
+++ app/src/main/java/br/com/fiap/challangedb1/screens/LoginScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -25,9 +22,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.fiap.challangedb1.R
 import br.com.fiap.challangedb1.components.Botao
 import br.com.fiap.challangedb1.components.BotaoOutline
+import br.com.fiap.challangedb1.components.CardTemplate
 import br.com.fiap.challangedb1.components.InputBox
 import br.com.fiap.challangedb1.components.TemplateScreen
 
@@ -59,12 +56,7 @@ fun LoginForm(
     updatedSenha: (String) -> Unit
 ) {
 
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-        colors = CardDefaults
-            .cardColors(colorResource(id = R.color.cor_card)),
-        elevation = CardDefaults.cardElevation(4.dp)
-    ) {
+    CardTemplate {
         Text(text = "Bem Vindo!",
             modifier = Modifier
                 .fillMaxWidth(),
