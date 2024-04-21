1944b5123ee2738008f4ac603bde84723d38bf68
yurimaretti <yurimaretti@hotmail.com>
2024-04-21 11:45:07 -0300

    Refatoração da tela de login e componente Header. Foi criada tela 'template' que será usada para todas as demais telas do projeto

---
+++ app/src/main/java/br/com/fiap/challangedb1/components/TemplateScreen.kt
@@ -0,0 +1,90 @@
+package br.com.fiap.challangedb1.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.challangedb1.R
+
+@Composable
+fun TemplateScreen(nomeTela: String, tela: @Composable () -> Unit) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.verde_fundo))
+                    .padding(16.dp)
+            ) {
+                Row (modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Text(text = nomeTela,
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.preto_fundo)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.jigsaw),
+                        contentDescription = "Logo do app",
+                        modifier = Modifier.size(60.dp)
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset(y = (-10).dp)
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(modifier = Modifier
+                    .padding(16.dp)
+                ) {
+                    tela()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TemplateScreenPreview() {
+    TemplateScreen(nomeTela = "Teste"){
+
+    }
+}
